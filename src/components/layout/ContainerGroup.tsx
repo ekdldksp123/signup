cd1b13cfbc82611df4bcfcec0642c839b3fafd19
ekdldksp123 <ekdldksp123@naver.com>
2022-07-03 01:46:11 +0900

    :tada: project setting:

---
+++ src/components/layout/ContainerGroup.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
+
+interface ContainerProps {
+    children: ReactJSXElement
+}
+const theme = createTheme();
+
+export const ContainerCenter:React.FC<ContainerProps> = ({children}) => {
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+                >
+                {children}    
+                </Box>
+            </Container>
+        </ThemeProvider>
+    )
+}
